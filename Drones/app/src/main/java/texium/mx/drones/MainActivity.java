01af5b8dab7e1851e33393d5f81323ab3025ff15
Saurett <javiersaurett@gmail.com>
2016-01-12 18:26:42 -0600

    Map Integration Service
    Create a integration service from google maps

--- Drones/app/src/main/java/texium/mx/drones/MainActivity.java
+++ Drones/app/src/main/java/texium/mx/drones/MainActivity.java
@@ -1,11 +1,11 @@
 package texium.mx.drones;
 
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
 
         /**
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -27,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
          **/
+    }
+
+    private void attemptLogin() {
+        Intent intentNavigationDrawer = new Intent(MainActivity.this,NavigationDrawerActivity.class);
+        startActivity(intentNavigationDrawer);
     }
 
 }

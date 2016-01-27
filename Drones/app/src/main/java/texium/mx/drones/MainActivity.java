d450a460e2597ab72abfe6f62943794bbdc10f7b
Saurett <javiersaurett@gmail.com>
2016-01-27 16:09:02 -0600

    Main Activity
    update main activity

--- Drones/app/src/main/java/texium/mx/drones/MainActivity.java
+++ Drones/app/src/main/java/texium/mx/drones/MainActivity.java
@@ -4,10 +4,16 @@ package texium.mx.drones;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText usernameLogin;
+    EditText passwordLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,31 +21,63 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button loginButton = (Button) findViewById(R.id.login_button);
+        Button cleanButton = (Button) findViewById(R.id.clean_button);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
-        /**
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        usernameLogin = (EditText) findViewById(R.id.username_login);
+        passwordLogin = (EditText) findViewById(R.id.password_login);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-         **/
+        loginButton.setOnClickListener(this);
+        cleanButton.setOnClickListener(this);
     }
 
+    //Login action
     private void attemptLogin() {
-        Intent intentNavigationDrawer = new Intent(MainActivity.this,NavigationDrawerActivity.class);
-        startActivity(intentNavigationDrawer);
+
+        boolean cancel = false;
+
+        usernameLogin.setError(null);
+        passwordLogin.setError(null);
+
+        String username = usernameLogin.getText().toString();
+        String password = passwordLogin.getText().toString();
+
+        if (TextUtils.isEmpty(password)) {
+            passwordLogin.setError("This password is invalid");
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(username)) {
+            usernameLogin.setError("This field is required");
+            cancel = true;
+        }
+
+        if (cancel) {
+
+        } else {
+            Intent intentNavigationDrawer = new Intent(MainActivity.this,NavigationDrawerActivity.class);
+            cleanAllLogin();
+            startActivity(intentNavigationDrawer);
+        }
     }
 
+    //Clean all login values
+    private void cleanAllLogin() {
+
+        usernameLogin.setText(null);
+        passwordLogin.setText(null);
+    }
+
+
+    //All OnClick login actions
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_button:
+                attemptLogin();
+                break;
+            case R.id.clean_button:
+                cleanAllLogin();
+                break;
+        }
+    }
 }
